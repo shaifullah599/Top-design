412c6d58e66c64d61375faabb89fb57e1ba893f2
shaifullah599 <83369080+shaifullah599@users.noreply.github.com>
2021-12-22 18:10:17 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// banner slick slider
+
+
+// 
+
+$('.sec-6-dgn').slick({
+  centerMode: true,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+
+
+
+//  slider-3
+
+
+$('.student-design').slick({
+  slidesToShow: 4,
+  slidesToScroll: 4
+});
+
+var filtered = false;
+
+$('.js-filter').on('click', function(){
+  if (filtered === false) {
+    $('.filtering').slick('slickFilter',':even');
+    $(this).text('Unfilter Slides');
+    filtered = true;
+  } else {
+    $('.filtering').slick('slickUnfilter');
+    $(this).text('Filter Slides');
+    filtered = false;
+  }
+});
+
+
+
+
+
